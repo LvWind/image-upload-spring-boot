71e80187274adffd93d9fe885274052f2f93c5e8
LvWind <lvwind.shaw@gmail.com>
2017-11-02 15:56:12 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # image-upload-spring-boot
 A simple Spring Boot application for image uploading
+
+## Init
+```
+./mvnw clean install
+```
+
+## Use
+Use Multipart/form-data POST
+
+DataPart name: 'file'
+
+## Return value
+```json
+{
+  "status":200,
+  "message":"success",
+  "results":[
+    {"url":"http://0.0.0.0/uploads/1509496746189.jpg"},
+    {"url":"http://0.0.0.0/uploads/1509496734770.jpg"}]
+}
+```
---
+++ src/main/java/com/lvwind/upload/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.lvwind.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.MultipartConfigElement;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("5MB");
+		factory.setMaxRequestSize("50MB");
+//		factory.setLocation("./uploads");
+		return factory.createMultipartConfig();
+	}
+
+}
